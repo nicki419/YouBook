13cc0c28e4c1d153daaacc32ae093ba15f6502d4
Nick <niklas.braun30419@gmail.com>
2024-06-13 13:08:00 +0200

    first commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace YouBook.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace YouBook.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
