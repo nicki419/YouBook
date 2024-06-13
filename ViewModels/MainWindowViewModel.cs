f24dd30b70a4c2affa4000a33ea6de5eea9f724b
Nick <niklas.braun30419@gmail.com>
2024-06-13 13:22:41 +0200

    Some Window Setup

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,27 @@
-﻿namespace YouBook.ViewModels;
+﻿using System;
+using System.Runtime.InteropServices;
+using ReactiveUI;
+
+namespace YouBook.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public MainWindowViewModel() {
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+            TitleBarVisible = false;
+            WindowHeight = 490;
+        } else {
+            WindowHeight = 450;
+        }
+    }
+    private int windowHeight = 450;
+    public int WindowHeight {
+        get => windowHeight;
+        set => this.RaiseAndSetIfChanged(ref windowHeight, value);
+    }
+    private bool titleBarVisible = true;
+    public bool TitleBarVisible {
+        get => titleBarVisible;
+        set => this.RaiseAndSetIfChanged(ref titleBarVisible, value);
+    }
 }
