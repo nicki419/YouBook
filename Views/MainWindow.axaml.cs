2dd1b4544bd99b332f8adf2b14c6d2a53402ee1f
Nick <niklas.braun30419@gmail.com>
2024-06-14 21:15:39 +0200

    Added SukiUI

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,8 +1,9 @@
 using Avalonia.Controls;
+using SukiUI.Controls;
 
 namespace YouBook.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : SukiWindow
 {
     public MainWindow()
     {
